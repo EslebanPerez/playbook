3e7ca62510b7a9f63288a8553b5e7c5bbb97a2d9
EslebanPerez <eslebanperez@gmail.com>
2022-04-18 14:32:21 -0500

    Agregado ejemplo para crear clase vacía

---
+++ weekly_mission_2/example2/example1_2.js
@@ -0,0 +1,14 @@
+console.log("POO")
+
+//Objeto de js
+const woopa = {
+    name: "Woopa",
+    mission: "Node JS",
+    age: "12",
+    color: "pink"
+}
+//Crea una clase
+class Ajolonauta{
+
+}
+console.log(Ajolonauta)
