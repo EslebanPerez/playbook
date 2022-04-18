4683576b05f5159f086ca9e2a919e15634aa5178
EslebanPerez <eslebanperez@gmail.com>
2022-04-18 15:51:17 -0500

    Agregando ejemplo de como agregar un metodo a la clase

--- weekly_mission_2/example2/example1_2.js
+++ weekly_mission_2/example2/example1_2.js
@@ -8,12 +8,22 @@ const woopa_obj = {
     color: "pink"
 }
 //Crea una clase
+/**
+ 1. Como creat una clase.
+ 2. Como instanciar un objeto de una clase.
+ 3. Instanciar varios objetos de una misma clase/
+ 4. Como agregar el constructor para adjuntar atributos.
+ 5. Métodos
+ */
 class Ajolonauta{
     constructor(name, mission, age, color){
         this.name = name;
         this.mission = mission;
         this.age = age;
         this.color =color;
+    }
+    sayHello(){
+        return `Hey! Hello I'm ${this.name}!`
     }
 
 }
@@ -24,9 +34,10 @@ console.log(woopa_obj_js)
 
 const woopa = new Ajolonauta('Woopa', 'Node JS', 18, 'Pink') //Instanciar un objeto
 console.log(woopa)
+console.log(woopa.sayHello())
 
 const wooper = new Ajolonauta('Wooper', 'Java', 10, "Blue")
-console.log(wooper)
+console.log(wooper.name)
 
 
 
