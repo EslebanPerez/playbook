38018a5f32bc7f956e35267dec801db3d5d2fef1
EslebanPerez <eslebanperez@gmail.com>
2022-04-19 15:58:32 -0500

    Agregando ejemplo 3 - forEach Imprimir una lista en Mayusculas

---
+++ weekly_mission_2/example1/example0_3.js
@@ -0,0 +1,4 @@
+// Ejemplo 3: for Each para imprimir los países en letras mayúsculas
+const countries = ['Finland', 'Denmark', 'Swden', 'Norway', 'Iceland']
+console.log('Ejemplo 3: Imprimiendo la lista de países en mayúsculas')
+countries.forEach(country => console.log( country.toUpperCase() ))
