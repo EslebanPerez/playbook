e50f2b423dfc4b5082874d08f8bc2a32aba2827e
EslebanPerez <eslebanperez@gmail.com>
2022-04-19 17:51:02 -0500

    Renombrando archivo

--- weekly_mission_2/example1/example1_3.js
+++ weekly_mission_2/example1/example1_3.js
@@ -2,3 +2,4 @@
 const countries = ['Finland', 'Denmark', 'Swden', 'Norway', 'Iceland']
 console.log('Ejemplo 3: Imprimiendo la lista de países en mayúsculas')
 countries.forEach(country => console.log( country.toUpperCase() ))
+console.log(countries)
