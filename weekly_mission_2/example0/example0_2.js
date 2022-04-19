23080c655680b65edd578ddd9ada80f6049b60f0
EslebanPerez <eslebanperez@gmail.com>
2022-04-19 13:07:25 -0500

    Add and object with properties

--- weekly_mission_2/example0/example0_2.js
+++ weekly_mission_2/example0/example0_2.js
@@ -1,36 +1,9 @@
-console.log("Ejemplo 2: Creación de un objeto con propiedades")
+console.log('*Ejemplo 2*')
+console.log("*Crear un objeto con propiedades*")
 
-const explorer = {
-    name:"Esleban",
-    edad: 25,
-    mission: "NodeJs",
-    stack:['NodeJs', 'ReactJs', 'AngularJs'],
-    blog:{ //Esto es un objeto anidado
-        url: "github.com/eslebanperez/blog",
-        post: 5, 
-    } 
-
+const myObjetc2 = {
+    name: "Esleban",
+    age: 25
 }
 
-console.log(explorer)
-console.log("Accediendo a las propiedades del objeto")
-console.log("Hola soy el explorer " + explorer.name)
-console.log(`\nOtra manera de acceder a la propiedad`)
-console.log(`Hola soy el explorer ${explorer.name}`)
-
-
-console.log("Ejemplo 3: Objeto con métodos")
-const ajolonauta = {
-    name : "Woopa",
-    mission: "Node JS",
-    sayHelooExplorers:function(){
-        console.log("Soy el ajolonauta, qué onda!")
-    },
-    tellMeMore: function(){
-        return `Ajolonauta: ${this.name}` //Accede al contexto de este objeto
-    }
-}
-console.log("Ajolonauta")
-ajolonauta.sayHelooExplorers()
-console.log(ajolonauta.tellMeMore())
-//Structre of interpretaction
+console.log(myObjetc2)
