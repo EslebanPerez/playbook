99753e47f6dfe50f5baece22f2b60eecf47d3e9d
EslebanPerez <eslebanperez@gmail.com>
2022-04-19 21:40:07 -0500

    Agregando ejemplo 7 - Uso de filter para filtrar una lista de elementos

---
+++ weekly_mission_2/example1/example1_7.js
@@ -0,0 +1,12 @@
+//Ejemplo 7: uso de filter para filtrar una lista de elememtos
+const countries = ['Mexico', 'Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland', 'Haiti', 'Australia']
+const countriesContainLand = countries.filter((country) => country.includes('land'))
+
+console.log('Ejemplo 7: Uso de Filter para filtrar una lista de elementos')
+console.log(countriesContainLand)
+
+const countriesEndsbyia = countries.filter((country) => country.endsWith('ia'))
+console.log('\nEjemplo 7: Paise que terminan en ia')
+console.log(countriesEndsbyia)
+
+
