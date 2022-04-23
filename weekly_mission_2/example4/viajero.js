cfaf6373181823b15a912fa6d246185ef34ac073
EslebanPerez <eslebanperez@gmail.com>
2022-04-23 03:02:49 -0500

    Agrengando clase hija

---
+++ weekly_mission_2/example4/viajero.js
@@ -0,0 +1,12 @@
+import Explorer from "./explorer.js";
+
+export default class Viajero extends Explorer{
+    constructor(name, username, mission, cycle){
+        super(name, username, mission)
+        this.cycle = cycle
+    }
+    getGeneralInfo(){
+        let nameAndUserName = this.getNameUserName()
+        return `${nameAndUserName}, Ciclo: ${this.cycle}`
+    }
+}
